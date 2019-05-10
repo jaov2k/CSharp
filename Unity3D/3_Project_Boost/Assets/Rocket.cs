af814e0952adea29ca9afc7c67ada990e734d592
Jose Ortiz <jose@jaov2k.com>
2019-05-10 09:26:10 -0700

    3_PB_Audio

--- Unity3D/3_Project_Boost/Assets/Rocket.cs
+++ Unity3D/3_Project_Boost/Assets/Rocket.cs
@@ -7,11 +7,13 @@ using UnityEngine.Experimental.PlayerLoop;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,15 +27,23 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) //Can thrust without rotating
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            print("Rotating left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Rotating right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
