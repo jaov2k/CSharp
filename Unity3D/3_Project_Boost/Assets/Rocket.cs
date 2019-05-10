78dbe6f8d27d05d718bff12f187750f62b1594ec
Jose Ortiz <jose@jaov2k.com>
2019-05-10 08:10:34 -0700

    3_PB_Inputs

---
+++ Unity3D/3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //Can thrust without rotating
+        {
+            print("Thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating right");
+        }
+    }
+}
