c887bff818a3d83c352da19ab2f3edabb2d7bc4e
Jose Ortiz <jose@jaov2k.com>
2019-05-11 12:35:36 -0700

    3_PB_Delay

--- Unity3D/3_Project_Boost/Assets/Rocket.cs
+++ Unity3D/3_Project_Boost/Assets/Rocket.cs
@@ -1,55 +1,71 @@
 ﻿using UnityEngine;
-using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.SceneManagement;
 
-public class Rocket : MonoBehaviour
-{
-    [SerializeField] float rcsThrust = 175f;
+public class Rocket : MonoBehaviour {
+
+    // todo fix lighting bug
+    [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+	// Use this for initialization
+	void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // todo somewhere stop sound on death
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        ProcessInput();
-    }
-
-    private void ProcessInput()
-    {
-        Thrusting();
-        Rotate();
-    }
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) { return; } // ignore collisions when dead
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
+                // do nothing
                 break;
             case "Finish":
-                print("Hit Finish");
-                SceneManager.LoadScene(1);
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f); // parameterise time
                 break;
             default:
-                print("DEAD!");
-                SceneManager.LoadScene(0);
+                print("Hit something deadly");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f); // parameterise time
                 break;
         }
     }
-    private void Thrusting()
+
+    private void LoadNextLevel()
     {
-        if (Input.GetKey(KeyCode.Space)) //Can thrust without rotating
+        SceneManager.LoadScene(1); // todo allow for more than 2 levels
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying)
+            if (!audioSource.isPlaying) // so it doesn't layer
             {
                 audioSource.Play();
             }
@@ -59,12 +75,12 @@ public class Rocket : MonoBehaviour
             audioSource.Stop();
         }
     }
+
     private void Rotate()
     {
-        rigidBody.freezeRotation = true; //take manual control of rotation
-        
+        rigidBody.freezeRotation = true; // take manual control of rotation
+       
         float rotationThisFrame = rcsThrust * Time.deltaTime;
-
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * rotationThisFrame);
@@ -74,8 +90,6 @@ public class Rocket : MonoBehaviour
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
-        rigidBody.freezeRotation = false; //resume physics control of rotation
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
-
-    
 }
